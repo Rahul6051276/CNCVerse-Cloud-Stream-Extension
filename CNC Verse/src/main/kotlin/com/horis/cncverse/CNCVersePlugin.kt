0ed39780710d59835e764948e15fe58214f73b7f
Rahul6051276 <techsmart397@gmail.com>
2026-03-29 11:42:09 +0530

    Update CNCVersePlugin.kt

--- CNC Verse/src/main/kotlin/com/horis/cncverse/CNCVersePlugin.kt
+++ CNC Verse/src/main/kotlin/com/horis/cncverse/CNCVersePlugin.kt
@@ -3,6 +3,7 @@ package com.horis.cncverse
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
+import com.horis.cncverse.BollyflixProvider
 
 @CloudstreamPlugin
 open class CNCVersePlugin: Plugin() {
@@ -13,9 +14,7 @@ open class CNCVersePlugin: Plugin() {
         PrimeVideoMirrorProvider.context = context
         HotStarMirrorProvider.context = context
         
-        // आपका नया Bollyflix यहाँ जुड़ गया है
         registerMainAPI(BollyflixProvider())
-        
         registerMainAPI(NetflixMirrorProvider())
         registerMainAPI(PrimeVideoMirrorProvider())
         registerMainAPI(HotStarMirrorProvider())
