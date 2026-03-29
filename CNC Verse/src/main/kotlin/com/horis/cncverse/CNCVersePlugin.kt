798b7a7c93dbd7122ee79fd8a54de8c1b014875b
Rahul6051276 <techsmart397@gmail.com>
2026-03-29 11:30:49 +0530

    Update CNCVersePlugin.kt

--- CNC Verse/src/main/kotlin/com/horis/cncverse/CNCVersePlugin.kt
+++ CNC Verse/src/main/kotlin/com/horis/cncverse/CNCVersePlugin.kt
@@ -7,17 +7,15 @@ import android.content.Context
 @CloudstreamPlugin
 open class CNCVersePlugin: Plugin() {
     override fun load(context: Context) {
-        // यहाँ हमने सभी पुराने और नए Bollyflix को एक साथ जोड़ दिया है
         NetflixMirrorStorage.init(context.applicationContext)
         DisneyPlusProvider.context = context
         NetflixMirrorProvider.context = context
         PrimeVideoMirrorProvider.context = context
         HotStarMirrorProvider.context = context
         
-        // 1. आपका नया Bollyflix
+        // सबसे पहले Bollyflix को रजिस्टर करें
         registerMainAPI(BollyflixProvider())
         
-        // 2. आपके पुराने प्रोवाइडर्स
         registerMainAPI(NetflixMirrorProvider())
         registerMainAPI(PrimeVideoMirrorProvider())
         registerMainAPI(HotStarMirrorProvider())
