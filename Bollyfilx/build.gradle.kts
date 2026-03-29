e7efa40204bbb654a02e4a4908b6d2b6be6681d6
Rahul6051276 <techsmart397@gmail.com>
2026-03-29 13:36:21 +0530

    Update build.gradle.kts

--- Bollyfilx/build.gradle.kts
+++ Bollyfilx/build.gradle.kts
@@ -1,18 +1,27 @@
-version = 1 
+// use an integer for version numbers
+version = 44
+
 
 cloudstream {
     authors = listOf("Rahul6051276")
 
-    status = 1 
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
 
     tvTypes = listOf(
         "Movie",
-        "TvSeries"
+        "TvSeries",
+        "Anime"
     )
-    
     language = "hi"
-
-    iconUrl = "https://raw.githubusercontent.com/Rahul6051276/icons/main/bollyflix.png"
+//  https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://hdhub4u.gratis&size=64
+    iconUrl = "https://raw.githubusercontent.com/Rahul6051276/TVVVV/refs/heads/main/Icons/Bollyfilx.png"
 
     isCrossPlatform = false
 }
