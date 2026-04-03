3534f97894491077b8cb2db63e17380baf3e402e
Rahul6051276 <techsmart397@gmail.com>
2026-04-03 14:46:07 +0530

    Update build.gradle.kts

--- Bollyfilx/build.gradle.kts
+++ Bollyfilx/build.gradle.kts
@@ -1,24 +1,24 @@
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 import org.gradle.api.Project
 
-// इसे 48 कर देते हैं ताकि ऐप को नया अपडेट मिले
-val versionNum = 48
+// इसे 51 कर देते हैं ताकि ऐप को एकदम ताज़ा अपडेट मिले
+val versionNum = 51
 
 cloudstream {
-    // यहाँ 'version' लिखना ज़रूरी है, वरना वह -1 उठा लेता है
+    // यहाँ वर्जन अपडेट करना सबसे ज़रूरी है
     version = versionNum 
     
     authors = listOf("Rahul6051276")
-    description = "Bollyflix"
+    description = "Bollyflix Working Plugin v-51"
     language = "hi"
     status = 1 
 
     tvTypes = listOf(
         "Movie",
-        "TvSeries",
-        "Anime"
+        "TvSeries"
     )
 
+    // आपका आइकॉन लिंक एकदम सही है
     iconUrl = "https://raw.githubusercontent.com/Rahul6051276/TVVVV/refs/heads/main/Icons/Bollyfilx.png"
     isCrossPlatform = false
 }
