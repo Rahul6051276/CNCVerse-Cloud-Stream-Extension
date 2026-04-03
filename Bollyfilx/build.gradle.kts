09a1a87becade8a26eaebf2cdd426b329c6337b0
Rahul6051276 <techsmart397@gmail.com>
2026-04-03 11:01:23 +0530

    Update build.gradle.kts

--- Bollyfilx/build.gradle.kts
+++ Bollyfilx/build.gradle.kts
@@ -1,37 +1,29 @@
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 import org.gradle.api.Project
 
-// वर्जन नंबर 45 कर दिया है ताकि नया अपडेट लोड हो
+// वर्जन 45 एकदम सही है
 val version = 45
 
 cloudstream {
-    // यहाँ आपका नाम
     authors = listOf("Rahul6051276")
-
     description = "Bollyflix"
     language = "hi"
-
-    /**
-     * Status: 1 (Ok)
-     * */
     status = 1 
 
-    // Bollyflix के हिसाब से कैटेगरी
     tvTypes = listOf(
         "Movie",
         "TvSeries",
         "Anime"
     )
 
-    // आपका दिया हुआ आइकॉन लिंक
     iconUrl = "https://raw.githubusercontent.com/Rahul6051276/TVVVV/refs/heads/main/Icons/Bollyfilx.png"
-    
-    // मोबाइल और टीवी दोनों के लिए
     isCrossPlatform = false
 }
 
-// यह हिस्सा आपके 'BuildConfig' वाले एरर को ठीक करेगा
 android {
+    // यह लाइन डालना ज़रूरी है ताकि ऐप को पता चले कि फोल्डर कहाँ है
+    namespace = "com.Bollyflix" 
+    
     buildFeatures {
         buildConfig = true
     }
