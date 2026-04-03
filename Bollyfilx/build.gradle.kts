80528e485934412d094a7e220811d3f3b4885399
Rahul6051276 <techsmart397@gmail.com>
2026-04-03 23:02:09 +0530

    Update build.gradle.kts

--- Bollyfilx/build.gradle.kts
+++ Bollyfilx/build.gradle.kts
@@ -1,11 +1,20 @@
-Version = 31
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 
-cloudstream {
+// 'v' छोटा होना चाहिए और '31' कोट्स में, यही नियम है
+version = "31"
+
+configure<CloudstreamExtension> {
     language = "hi"
     description = "Movies and Series upto 4K"
-    authors = listOf("Bollyfilx")
+    authors = listOf("Rahul6051276")
 
-    status = 1 
+    /**
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1
     tvTypes = listOf(
         "TvSeries",
         "Movie",
@@ -13,6 +22,6 @@ cloudstream {
         "Anime"
     )
 
-    // सुधारी हुई लिंक नीचे है:
-    iconUrl = "https://raw.githubusercontent.com/Rahul6051276/CNCVerse-Cloud-Stream-Extension/master/Bollyflix/icon.png"
+    // आपकी अपनी रिपॉजिटरी का आइकॉन लिंक
+    iconUrl = "https://raw.githubusercontent.com/Rahul6051276/CNCVerse-Cloud-Stream-Extension/master/Bollyfilx/icon.png"
 }
