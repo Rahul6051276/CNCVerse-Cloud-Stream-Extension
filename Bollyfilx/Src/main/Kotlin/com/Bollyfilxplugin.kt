8d844f5727a200c01c8cfc830611910731b89fcf
Rahul6051276 <techsmart397@gmail.com>
2026-04-03 10:26:09 +0530

    Update Bollyfilxplugin.kt

--- Bollyfilx/Src/main/Kotlin/com/Bollyfilxplugin.kt
+++ Bollyfilx/Src/main/Kotlin/com/Bollyfilxplugin.kt
@@ -1,4 +1,4 @@
-package com.Bollyfilx
+package com.Bollyflix  // यह नाम Bollyflix.kt वाले से मैच होना चाहिए
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
@@ -7,7 +7,7 @@ import android.content.Context
 @CloudstreamPlugin
 class BollyflixPlugin: Plugin() {
     override fun load(context: Context) {
-        // यहाँ 'Bollyflix()' वही नाम है जो आपने अपनी Provider फाइल में रखा है
+        // यहाँ हम Bollyflix वाली मशीन को ऐप में चालू कर रहे हैं
         registerMainAPI(Bollyflix()) 
     }
 }
