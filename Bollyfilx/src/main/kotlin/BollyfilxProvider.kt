7be6c5264e2cee1e5424c42fb68f716dcbf3716d
Rahul6051276 <techsmart397@gmail.com>
2026-04-04 10:41:28 +0530

    Update BollyfilxProvider.kt

--- Bollyfilx/src/main/kotlin/BollyfilxProvider.kt
+++ Bollyfilx/src/main/kotlin/BollyfilxProvider.kt
@@ -6,14 +6,9 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 @CloudstreamPlugin
 class BollyflixProvider: BasePlugin() {
     override fun load() {
-        // यह आपके इंजन को रजिस्टर करता है
         registerMainAPI(BollyFlix())
-        
-        // वीडियो चलाने के लिए जरूरी Extractors
-        registerExtractorAPI(Filesdl())
         registerExtractorAPI(GDFlix())
-        registerExtractorAPI(HubCloud())
+        registerExtractorAPI(LinksMod())
         registerExtractorAPI(Gofile())
-        registerExtractorAPI(SharedDrive())
     }
 }
