534c6363a64892de743c10e00110683ba2f5b639
Rahul6051276 <techsmart397@gmail.com>
2026-04-04 10:01:05 +0530

    Update build.gradle.kts

--- Bollyfilx/build.gradle.kts
+++ Bollyfilx/build.gradle.kts
@@ -1,8 +1,28 @@
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+import com.android.build.gradle.BaseExtension
 
-// 'v' छोटा होना चाहिए और '31' कोट्स में, यही नियम है
+// वर्जन और बेसिक जानकारी
 version = "31"
 
+// एंड्रॉइड सेटिंग्स (यहीं पर एरर था, जिसे मैंने ठीक कर दिया है)
+configure<BaseExtension> {
+    compileSdkVersion(34)
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+    }
+
+    // यही वह हिस्सा है जो आपके 'Build Failed' एरर को ठीक करेगा
+    buildFeatures.buildConfig = true
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+// क्लाउडस्ट्रीम प्लगइन की सेटिंग्स
 configure<CloudstreamExtension> {
     language = "hi"
     description = "Movies and Series upto 4K"
@@ -22,6 +42,6 @@ configure<CloudstreamExtension> {
         "Anime"
     )
 
-    // आपकी अपनी रिपॉजिटरी का आइकॉन लिंक
+    // आपका अपना आइकॉन लिंक
     iconUrl = "https://raw.githubusercontent.com/Rahul6051276/CNCVerse-Cloud-Stream-Extension/master/Bollyfilx/icon.png"
 }
